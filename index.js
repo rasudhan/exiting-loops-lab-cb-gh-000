03565f5ca7c79f8d2e731ac5d4b19636829415b8
Anandha Sudhan <rasudhan@gmail.com>
2018-10-05 00:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ function keepGoing(array, changeValue, skipValue){
 
 function findBy(array, findFn) {
 
-  for (let i = 0,  i < array.length; i++) {
+  for (let i = 0, i < array.length; i++) {
       if (findFn(array[i])) {
         return array[i]
       }
     }
-  
+
     return null
-  
+
 }
